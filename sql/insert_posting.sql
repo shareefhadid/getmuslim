c5c99c0d689e0b437b6dd39a0d3fe75be6c2fea0
shareefhadid <shareefhadid@gmail.com>
2024-09-22 17:55:49 -0400

    updated sql

--- sql/insert_posting.sql
+++ sql/insert_posting.sql
@@ -4,4 +4,17 @@ VALUES (
     'Clearoute Inc. is a full-service agency helping you achieve unparalleled success in marketing, branding, and web development. Let us help you see the future.',
     'POINT(-79.374347 43.842077)',
     '155 Commerce Valley Dr E, Thornhill, ON L3T 7X6, Canada'
-  );
+  ),
+  (
+    'Omars Shoes',
+    'A family shoe store in Newmarket & Aurora.',
+    'POINT(-79.447417 44.016803)',
+    '15620 Bayview Ave Unit 8-9, Aurora, ON L4G 0Y7, Canada'
+  ),
+  (
+    'Mawlana Kabab',
+    'Delicious Afghan and Turkish cuisine.',
+    'POINT(79.446827 44.010638)',
+    '15440 Bayview Ave unit B1, Aurora, ON L4G 7J1, Canada'
+  ),
+;
