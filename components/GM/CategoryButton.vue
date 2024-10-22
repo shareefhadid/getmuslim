12f5f1b400891b802ba35c68e7a3170179636ed6
shareefhadid <shareefhadid@gmail.com>
2024-10-21 22:09:41 -0400

    added some logic for category selection

--- components/GM/CategoryButton.vue
+++ components/GM/CategoryButton.vue
@@ -1,10 +1,31 @@
 <template>
-	<UButton variant="outline" size="xs" :icon="icon" color="neutral">{{ label }}</UButton>
+	<UButton :variant="isActive ? 'solid' : 'outline'" size="xs" :icon="icon" color="neutral" @click="handleClick"
+					 class="hover:cursor-pointer">
+		{{ label }}
+	</UButton>
 </template>
 
 <script lang="ts" setup>
-const { icon, label } = defineProps<{
+const { icon, label, id } = defineProps<{
 	icon?: string;
 	label: string;
+	id?: string | null;
 }>();
+
+const router = useRouter();
+const route = useRoute();
+
+const selectedCategory = computed(() => route.query.category);
+
+const handleClick = async () => {
+	if (id && selectedCategory.value !== id) {
+		await navigateTo({ query: { category: id }, replace: true });
+	} else {
+		await navigateTo({ query: {}, replace: true })
+	}
+};
+
+const isActive = computed(() => {
+	return selectedCategory.value === id;
+});
 </script>
--- components/GM/CategoryCarousel.vue
+++ components/GM/CategoryCarousel.vue
@@ -1,15 +1,27 @@
 <template>
-  <UCarousel v-slot="{ item }" :items="categories ?? undefined"
+  <UCarousel v-slot="{ item }" :items="items"
              :ui="{ item: 'not-first:pl-3 basis-auto last:me-4', container: 'p-px max-sm:px-4' }" wheel-gestures
              drag-free>
-    <GMCategoryButton :icon="item.icon ?? undefined" :label="item.label" />
+    <GMCategoryButton :icon="item.icon ?? undefined" :label="item.label" :id="item.id?.toString()" />
   </UCarousel>
 </template>
 
 <script lang="ts" setup>
+type CategoryCarouselItems = {
+  icon?: string | null;
+  id?: number;
+  label: string;
+  parent_id?: number | null;
+}[]
+
 const { data } = await useFetch("/api/categories", {
   headers: useRequestHeaders(["cookie"]),
 });
 
 const categories = data.value?.categories;
+
+const items: CategoryCarouselItems = [
+  { label: "All" },
+  ...categories ?? [],
+];
 </script>
