f34fb51c1721a05e9af845403b7ca6096324961b
shareefhadid <shareefhadid@gmail.com>
2025-03-31 23:55:28 -0400

    seo improvements

--- app.vue
+++ app.vue
@@ -25,6 +25,37 @@ useHead({
       title: "getmuslim RSS Feed",
       href: "/feed.xml",
     },
+    { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: "/favicon-16x16.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/favicon-32x32.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/apple-touch-icon.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "192x192",
+      href: "/android-chrome-192x192.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "512x512",
+      href: "/android-chrome-512x512.png",
+    },
+    { rel: "manifest", href: "/site.webmanifest" },
   ],
 });
 
@@ -37,7 +68,12 @@ useSchemaOrg([
     publisher: {
       "@type": "Organization",
       name: "getmuslim",
-      logo: `${useRuntimeConfig().public.siteUrl}/logo.png`,
+      logo: {
+        "@type": "ImageObject",
+        url: `${useRuntimeConfig().public.siteUrl}/logo.png`,
+        width: 512,
+        height: 512
+      },
     },
   }),
 ]);
