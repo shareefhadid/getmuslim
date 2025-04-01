26a8c7369a98735a5749b8b5940b4701f9702acf
shareefhadid <shareefhadid@gmail.com>
2025-03-31 23:31:48 -0400

    fixed img util, upgrade packages

--- utils/formatPostingImagePath.ts
+++ utils/formatPostingImagePath.ts
@@ -1,4 +1,2 @@
 export const formatPostingImagePath = (path: string) =>
-  path.startsWith("images/")
-    ? `${useRequestURL().origin}/images/image-502f2a8a.png`
-    : path;
+  path.startsWith("images/") ? `${useRequestURL().origin}/${path}` : path;
