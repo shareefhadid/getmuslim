1675957ec1f60227737b09b62ac2ab09375901fd
shareefhadid <shareefhadid@gmail.com>
2025-05-13 17:03:38 -0400

    drop functions, fix nearby function

---
+++ supabase/migrations/20250513205514_drop_old_posting_functions.sql
@@ -0,0 +1,90 @@
+DROP FUNCTION IF EXISTS public.get_recent_postings(
+  double precision,
+  double precision,
+  bigint,
+  integer,
+  integer
+);
+DROP FUNCTION IF EXISTS public.get_recent_postings_v2(
+  double precision,
+  double precision,
+  bigint,
+  integer,
+  integer
+);
+DROP FUNCTION IF EXISTS public.get_recent_postings_v3(
+  double precision,
+  double precision,
+  bigint,
+  integer,
+  integer
+);
+DROP FUNCTION IF EXISTS public.get_recent_postings_v4(
+  double precision,
+  double precision,
+  bigint,
+  integer,
+  integer
+);
+DROP FUNCTION IF EXISTS public.get_recent_postings_v5(
+  double precision,
+  double precision,
+  bigint,
+  integer,
+  integer
+);
+DROP FUNCTION IF EXISTS public.get_recent_postings_v6(
+  double precision,
+  double precision,
+  bigint,
+  integer,
+  integer
+);
+DROP FUNCTION IF EXISTS public.get_nearby_postings(
+  double precision,
+  double precision,
+  bigint,
+  bigint,
+  integer,
+  integer
+);
+DROP FUNCTION IF EXISTS public.get_nearby_postings_v2(
+  double precision,
+  double precision,
+  bigint,
+  bigint,
+  integer,
+  integer
+);
+DROP FUNCTION IF EXISTS public.get_nearby_postings_v3(
+  double precision,
+  double precision,
+  bigint,
+  bigint,
+  integer,
+  integer
+);
+DROP FUNCTION IF EXISTS public.get_nearby_postings_v4(
+  double precision,
+  double precision,
+  bigint,
+  bigint,
+  integer,
+  integer
+);
+DROP FUNCTION IF EXISTS public.get_nearby_postings_v5(
+  double precision,
+  double precision,
+  bigint,
+  bigint,
+  integer,
+  integer
+);
+DROP FUNCTION IF EXISTS public.get_nearby_postings_v6(
+  double precision,
+  double precision,
+  bigint,
+  bigint,
+  integer,
+  integer
+);
